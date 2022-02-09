bbb3dd3969dc701c1e5c3e502b6c90c2493fd4c6
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2022-02-09 06:55:14 +0530

    Split the functionality

--- checker.c
+++ checker.c
@@ -1,20 +1,63 @@
 #include <stdio.h>
 #include <assert.h>
+int temperatureRangeIsOk(float);
+int SocIsOk(float);
+int chargeRateIsOk(float);
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
+ int ret_batt_stat;
+ if(ret_batt_stat=temperatureRangeIsOk(temperature))!=True)
+   return ret_batt_stat;
+ else if(ret_batt_stat=SocIsOk(soc))!=True)
+    return ret_batt_stat;
+ else if(ret_batt_stat=chargeRateIsOk(chargeRate))!=True)
+    return ret_batt_stat;
   return 1;
 }
 
+int temperatureRangeIsOk(float temperature)
+{
+  int temperature_stat=1;
+   if(temperature < 0) 
+   {
+       printf("Temperature is lesser than minimum and is out of range!\n");
+       temperature_stat=0;
+   }
+  else if(temperature > 45) 
+   {
+       printf("Temperature is greater than maximum and is out of range!\n");
+       temperature_stat=0;
+   }
+  return temperature_stat;
+}
+
+int SocIsOk(float soc)
+{
+  int soc_stat=1;
+   if(soc < 20) 
+   {
+       printf("Soc is lesser than minimum and is out of range!\n");
+       soc_stat=0;
+   }
+  else if(temperature > 80) 
+   {
+       printf("Soc is greater than maximum and is out of range!\n");
+       soc_stat=0;
+   }
+  return soc_stat;
+}
+
+int chargeRateIsOk(float chargeRate)
+{
+  int chargeRate_stat=1;
+   if(chargeRate > 0.8) 
+   {
+       printf("chargeRate is out of range!\n");
+       chargeRate_stat=0;
+   }
+  return chargeRate_stat;
+}
+  
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
